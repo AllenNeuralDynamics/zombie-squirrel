76682a73699913d0fc7e737d06fc3d0493c65e44
Dan Birman <danbirman@gmail.com>
2025-11-01 12:52:23 -0700

    feat: improve integration test

--- scripts/integration_tests.py
+++ scripts/integration_tests.py
@@ -2,6 +2,8 @@
 Integration test for scurry_project_names() using Redshift backend.
 """
 import os
+import time
+
 from zombie_squirrel import unique_project_names
 
 
@@ -9,8 +11,10 @@ def main():
     # Set environment variable to use Redshift backend
     os.environ["TREE_SPECIES"] = "redshift"
     # Run the function
-    project_names = unique_project_names(force_update=True)
-    print("Project names:", project_names)
+    start = time.time()
+    project_names = unique_project_names()
+    end = time.time()
+    print(f"Fetched {len(project_names)} project names in {end - start:.2} seconds.")
     assert isinstance(project_names, list), "Result should be a list"
     print("Integration test passed.")
 
