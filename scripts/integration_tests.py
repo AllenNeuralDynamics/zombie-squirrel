b98397b4cdc447f288d5b35c7a2e0c874bbd3f96
Dan Birman <danbirman@gmail.com>
2025-11-01 22:14:12 -0700

    chore: lint

--- scripts/integration_tests.py
+++ scripts/integration_tests.py
@@ -1,12 +1,16 @@
 """
 Integration test for scurry_project_names() using Redshift backend.
 """
+
 import logging
 import os
 import time
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
 from zombie_squirrel import unique_project_names, unique_subject_ids
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s"
+)
 
 
 def _test_redshift_unique_project_names():
@@ -16,7 +20,9 @@ def _test_redshift_unique_project_names():
     start = time.time()
     project_names = unique_project_names()
     end = time.time()
-    logging.info(f"Fetched {len(project_names)} project names in {end - start:.2} seconds.")
+    logging.info(
+        f"Fetched {len(project_names)} project names in {end - start:.2} seconds."
+    )
     assert isinstance(project_names, list), "Result should be a list"
 
 
@@ -27,7 +33,9 @@ def _test_redshift_unique_subject_ids():
     start = time.time()
     subject_ids = unique_subject_ids()
     end = time.time()
-    logging.info(f"Fetched {len(subject_ids)} subject IDs in {end - start:.2} seconds.")
+    logging.info(
+        f"Fetched {len(subject_ids)} subject IDs in {end - start:.2} seconds."
+    )
     assert isinstance(subject_ids, list), "Result should be a list"
 
 
