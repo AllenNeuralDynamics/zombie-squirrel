f97d7bcc755f527df892f9000968150a6f6912b8
dyf <david.feng@alleninstitute.org>
2026-05-07 17:23:44 -0700

    public url, neuroglass links

--- src/zombie_squirrel/acorn_helpers/assets_smartspim.py
+++ src/zombie_squirrel/acorn_helpers/assets_smartspim.py
@@ -4,6 +4,8 @@ import logging
 
 import boto3
 import pandas as pd
+from botocore import UNSIGNED
+from botocore.config import Config
 from aind_data_access_api.document_db import MetadataDbClient
 
 import zombie_squirrel.acorns as acorns
@@ -12,7 +14,7 @@ from zombie_squirrel.acorn_helpers.source_data import source_data
 from zombie_squirrel.squirrel import Column
 from zombie_squirrel.utils import SquirrelMessage, setup_logging
 
-NEUROGLANCER_BASE = "https://neuroglancer-demo.appspot.com/#!"
+NEUROGLANCER_BASE = "https://allen.neuroglass.io/new#!"
 AIND_OPEN_DATA_BUCKET = "aind-open-data"
 
 
